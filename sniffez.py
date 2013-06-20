7b1b2642387f85f1dff0958411d1b9865ced7a86
tmap <cristian@mariolini.net>
2013-06-20 23:04:58 +0200

    keep it simple stupid

--- sniffez.py
+++ sniffez.py
@@ -14,15 +14,9 @@ value_regex = re.compile("GET.*\\\\r\\\\n\\\\r\\\\n")
 out_file = csv.writer(open('file_out.csv', "wb"))
 
 def sniffer():
-	try:
-		while True:
-			a=sniff(filter="tcp port 80 and ip[2:2] > 40 and tcp[tcpflags] & tcp-push != 0 and dst port 80",count=1)
-			m = value_regex.findall(str(a))
-			if m:
-				out_file.writerow(m)
-			status_sniff = raw_input('Running...[type "stop" to quit]: ')
-			if status_sniff == 'stop':
-				exit(1)
-	except:
-		pass
+	while True:
+		a=sniff(filter="tcp port 80 and ip[2:2] > 40 and tcp[tcpflags] & tcp-push != 0 and dst port 80",count=1)
+		m = value_regex.findall(str(a))
+		if m:
+			out_file.writerow(m)
 sniffer()
