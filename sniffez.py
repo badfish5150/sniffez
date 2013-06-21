be79700347ea76bfb5b69ce3cc394ab40d9ae221
tmap <cristian@mariolini.net>
2013-06-21 19:11:53 +0200

    mysql update, now storing logs with readable newlines

--- sniffez.py
+++ sniffez.py
@@ -11,28 +11,28 @@ import csv
 import sys
 import MySQLdb as mdb
 
-
+# set local database
+db_local_user = 'root'
+db_local_pass = 'db_pass'
+db_local_address = 'localhost'
+db_local_schema = 'sniffez'
+con = None
+con = mdb.connect(db_local_address, db_local_user,db_local_pass, db_local_schema);
+cur = con.cursor()
+sql="INSERT INTO log(log_data) VALUES (%s)"
 x=0
 value_regex = re.compile("GET.*\\\\r\\\\n\\\\r\\\\n")
 
 def sniffer():
-	# set local database
-	db_local_user = 'db_user'
-	db_local_pass = 'yourpwd'
-	db_local_address = 'localhost'
-	db_local_schema = 'sniffez'
-	con = None
-	con = mdb.connect(db_local_address, db_local_user,db_local_pass, db_local_schema);
-	cur = con.cursor()
-	sql="INSERT INTO log(log_data) VALUES (%s)"
 	while True:
 		a=sniff(filter="tcp port 80 and ip[2:2] > 40 and tcp[tcpflags] & tcp-push != 0 and dst port 80",count=1)
-		m = value_regex.findall(str(a))
-		if m:
+		base_request = value_regex.findall(str(a))
+		request_str = [request_str.replace('\\r\\n','\n') for request_str in base_request]
+		if request_str:
 			try:
-				cur.execute(sql,(m))
+				cur.execute(sql,(request_str))
 				con.commit()
-				print m
+				print request_str
 			except mdb.Error as e:
 				print '\033[1;31m[FAIL] Please check\033[1;m', 'Error %d: %s' % (e.args[0], e.args[1])
 
