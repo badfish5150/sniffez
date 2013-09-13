e51fd8d7364fff59698526a5626334a1916d82da
Cristian Mariolini <cristian@mariolini.net>
2013-09-13 22:12:03 +0200

    Update sniffez.py

--- sniffez.py
+++ sniffez.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-# Sniffez v0.2b sqlite3
+# Sniffez v0.2c sqlite3
 # Created by: Cristian 'Tmap' Mariolini
 # Mail: cristian [at] mariolini [dot] net
 # Twitter: @mariolinic
 # Web: https://www.mariolini.net
-
+# NOTE . YOU NEED TO CREATE A SQLITE DATABASE CALLED "sniffez.db"  IN THE SAME DIRECTORY AS SNIFFEZ
+# CREATE THE DB WITH 2 COLUMNS log_id (PK AUTOINCREMENTAL, NOT NULL) AND log_data (TEXT)
 from scapy.all import *
 import re
 import csv
@@ -12,23 +13,25 @@ import sys
 import sqlite3
 con=sqlite3.connect('sniffez.db')
 cur=con.cursor()
-sql="INSERT INTO log(log_data) VALUES (%s)"
+
 x=0
 value_regex = re.compile("GET.*\\\\r\\\\n\\\\r\\\\n")
 
+
 def sniffer():
 	global x
-	while x<10:
+	while x<100:
 		a=sniff(filter="tcp port 80 and ip[2:2] > 40 and tcp[tcpflags] & tcp-push != 0 and dst port 80",count=1)
 		base_request = value_regex.findall(str(a))
 		request_str = [request_str.replace('\\r\\n','\n') for request_str in base_request]
 		if request_str:
 			try:
-				cur.execute(sql,(request_str))
-				print request_str
+				cur.execute("""INSERT INTO sniffez_log VALUES (NULL,?)""",(request_str))
+				con.commit()
+				print request_str[0]
+				
 			except sqlite3.Error as e:
-				print '\033[1;31m[FAIL] Please check\033[1;m', 'Error %d: %s' % (e.args[0], e.args[1])
+				print e.args[0]
 		x=x+1
 sniffer()
-
 
