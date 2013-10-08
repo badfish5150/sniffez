176fd166c90cb5c98ec8844112bdd54461bc8416
Cristian Mariolini <cristian@mariolini.net>
2013-10-08 14:47:49 +0200

    Update sniffez.py

--- sniffez.py
+++ sniffez.py
@@ -4,34 +4,44 @@
 # Mail: cristian [at] mariolini [dot] net
 # Twitter: @mariolinic
 # Web: https://www.mariolini.net
-# NOTE . YOU NEED TO CREATE A SQLITE DATABASE CALLED "sniffez.db"  IN THE SAME DIRECTORY AS SNIFFEZ
-# CREATE THE DB WITH 2 COLUMNS log_id (PK AUTOINCREMENTAL, NOT NULL) AND log_data (TEXT)
+
 from scapy.all import *
 import re
 import csv
 import sys
 import sqlite3
+import datetime
+import time
 con=sqlite3.connect('sniffez.db')
 cur=con.cursor()
-
-x=0
-value_regex = re.compile("GET.*\\\\r\\\\n\\\\r\\\\n")
+def create_db():
+	global cur
+	try:
+		create_db="""CREATE TABLE "sniffez_log" (
+			"log_id" INTEGER PRIMARY KEY DEFAULT (1),
+			"log_time" INTEGER NOT NULL,
+			"log_data" TEXT NOT NULL
+		);"""
+		cur.execute(create_db)
+	except sqlite3.Error as e:
+				print e.args[0]
 
 
 def sniffer():
-	global x
-	while x<100:
+	x=0
+	while x<10:
 		a=sniff(filter="tcp port 80 and ip[2:2] > 40 and tcp[tcpflags] & tcp-push != 0 and dst port 80",count=1)
-		base_request = value_regex.findall(str(a))
-		request_str = [request_str.replace('\\r\\n','\n') for request_str in base_request]
-		if request_str:
-			try:
-				cur.execute("""INSERT INTO sniffez_log VALUES (NULL,?)""",(request_str))
-				con.commit()
-				print request_str[0]
-				
-			except sqlite3.Error as e:
-				print e.args[0]
-		x=x+1
-sniffer()
+		parse_get(a)
+	x=x+1
+def parse_get(a):
+	get_regex = re.compile("GET.*\\\\r\\\\n\\\\r\\\\n")
+	base_request = get_regex.findall(str(a))
+	request_str = [request_str.replace('\\r\\n','\n') for request_str in base_request]
+	print request_str
+def parse_post(a):
+	post_regex = re.compile ("POST.*\\\\r\\\\n\\\\r\\\\n.*\' \|>>>>\]")
+	base_request = post_regex.findall(str(a))
+	request_str = [request_str.replace('\\r\\n','\n') for request_str in base_request]
+	
 
+sniffer()	
