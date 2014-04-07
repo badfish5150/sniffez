1c75f0fccbfd4923db484d4106fe298d32323797
Cristian Mariolini <cristian@mariolini.net>
2014-04-07 14:36:12 +0200

    Update sniffez.py

--- sniffez.py
+++ sniffez.py
@@ -1,27 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-This is a simple http sniffer made in python 2.7
-
 Python module required => Scapy(http://www.secdev.org/projects/scapy/doc/index.html)
 
-Copyright® 2014 Alessandro Pucci - @b4d_tR1p ; Cristian 'Tmap' Mariolini - @mariolinic
+Cristian 'Tmap' Mariolini - @mariolinic
+Alessandro Pucci - @b4d_tR1p
 
-Date : 2014
-Licence : GPL v3 or any later version
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+Licence : GPL v3
 
 '''
 
